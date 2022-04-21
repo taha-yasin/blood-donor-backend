266e7699c809dd446bf28d40ed3f03add81720ec
tahayasin <gulamtahayaseen@gmail.com>
2022-04-21 14:56:20 +0530

    Send whatsapp messege when request is created

--- src/main/java/io/tahayasin/blood_donor/service/AppUserService.java
+++ src/main/java/io/tahayasin/blood_donor/service/AppUserService.java
@@ -118,7 +118,7 @@ public class AppUserService {
         appUser.setLastName(appUserDTO.getLastName());
         appUser.setDataOfBirth(appUserDTO.getDateOfBirth());
         appUser.setGender(appUserDTO.getGender());
-        appUser.setEnabled(false);
+        appUser.setEnabled(true);
         if (appUserDTO.getUserRoles() != null) {
             final List<AppRole> userRoles = appRoleRepository.findAllById(appUserDTO.getUserRoles());
             if (userRoles.size() != appUserDTO.getUserRoles().size()) {
