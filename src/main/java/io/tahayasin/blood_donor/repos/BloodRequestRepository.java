7dfeff47b7ea7321a084b033c67f9580468cbdac
tahayasin <gulamtahayaseen@gmail.com>
2022-05-18 20:35:04 +0530

    Add receivedRequests() to DonorService to get blood requests received by donor

--- src/main/java/io/tahayasin/blood_donor/repos/BloodRequestRepository.java
+++ src/main/java/io/tahayasin/blood_donor/repos/BloodRequestRepository.java
@@ -1,10 +1,15 @@
 package io.tahayasin.blood_donor.repos;
 
 import io.tahayasin.blood_donor.domain.BloodRequest;
+
+import java.util.List;
 import java.util.UUID;
+
+import io.tahayasin.blood_donor.domain.Donor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BloodRequestRepository extends JpaRepository<BloodRequest, UUID> {
+    List<BloodRequest> findAllByDonors(Donor donor);
 }
