5f9a78dd176ccca16949db98ad4d1fe38e681e33
Code-4ge <panmandneeraj@gmail.com>
2022-05-21 23:42:48 +0530

    Update findByBloodGroupAndCityOrPincode method in DonorRepository

--- src/main/java/io/tahayasin/blood_donor/repos/DonorRepository.java
+++ src/main/java/io/tahayasin/blood_donor/repos/DonorRepository.java
@@ -4,11 +4,16 @@ import io.tahayasin.blood_donor.domain.Donor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DonorRepository extends JpaRepository<Donor, Long> {
 
     //Page<Donor> findByBloodGroupAndCityOrPincode(String bloodGroup, String city, String pincode, Pageable pageable);
-    Page<Donor> findByBloodGroupAndCityOrPincode(String bloodGroup, String city, String pincode, Pageable pageable);
+
+    @Query("select d from Donor d where d.bloodGroup in :bloodGroupList and (d.city = :city or d.pincode = :pincode)")
+    Page<Donor> findByBloodGroupAndCityOrPincode(List<String> bloodGroupList, String city, String pincode, Pageable pageable);
 }
